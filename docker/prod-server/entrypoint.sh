8b13801ff0fb20c6283f454fd6a0a85060890c6d
vitaly512 <nimbleraccoonind@gmail.com>
2022-09-30 08:47:42 +0500

    - Updates/improvements made in docker files. - Added docker-compose implementation for prod environment. - New python packages added in requirements file.

--- docker/prod-server/entrypoint.sh
+++ docker/prod-server/entrypoint.sh
@@ -7,4 +7,4 @@ mkdir -p ./data/static ./data/media
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-uvicorn etebase_server.asgi:application --host 0.0.0.0 --port 3735
+uvicorn etebase_server.asgi:application --host 0.0.0.0 --port 80
